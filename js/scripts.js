21196e2483c9f53d994fb3ee5fedd5a0e6d04d60
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-04-17 09:26:30 -0700

    removed script from contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,63 @@
 function validateEmail() {
   let value = emailInput.value;
-  let hasAtSign = value.indexOf('@') > -1;
-  let hasDot =  value.indexOf('.') > -1;
-  return value && hasAtSign && hasDot;
+
+  if (!value) {
+    showErrorMessage(emailInput, 'Email is a required field.');
+    return false;
+  }
+
+  if (value.indexOf('@') === -1) {
+    showErrorMessage(emailInput, 'You must enter a valid email address.');
+    return false;
+  }
+
+  if (value.indexOf('.') === -1) {
+    showErrorMessage(emailInput, 'You must enter a valid email address.');
+    return false;
+  }
+
+  showErrorMessage(emailInput, null);
+  return true;
 }
+
+function showErrorMessage(input, message) {
+  let container = input.parentElement;
+
+  let error = container.querySelector('.error-message');
+  if (error) {
+    container.removeChild(error);
+  }
+
+  if (message) {
+    let error = document.createElement('div');
+    error.classList.add('error-message');
+    error.innerText = message;
+    container.appendChild(error);
+  }
+}
+
+function validateMessage() {
+  let value = messageInput.value;
+
+  if (!value) {
+    showErrorMessage(messageInput, 'Message is a required field.');
+    return false;
+  }
+
+  if (value.length < 1) {
+    showErrorMessage(messageInput, 'Please write a message.');
+    return false;
+  }
+
+  showErrorMessage(messageInput, null);
+  return true;
+}
+
+function validateForm() {
+  let isValidEmail = validateEmail();
+  let isValidMessage = validateMessage();
+  return isValidEmail && isValidMessage;
+}
+
+emailInput.addEventListener('input', validateEmail);
+passwordInput.addEventListener('input', validateMessage);
